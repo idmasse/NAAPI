b05d4013d520a7b87da10f70601c9c8141bb3c40
ianm <idmasse@icloud.com>
2024-01-29 12:42:43 -0800

    updates imports and Routes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,28 +2,30 @@ import './App.css';
 import { useState } from "react";
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import AuthPage from '../AuthPage/AuthPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
+import Apod from "../Apod/Apod";
+import Posts from '../Posts/Posts';
+import SearchArchive from '../SearchArchive/SearchArchive';
 
 export default function App() {
   const [user, setUser] = useState(getUser())
 
   return (
     <main className="App">
-      { 
-      user ?
-        <>
-          <NavBar user={user} setUser={setUser}/>
-          <Routes>
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
-          </Routes>
-        </>
-        :
-        <AuthPage setUser={setUser} />
-        }
+      {
+        user ?
+          <>
+            <NavBar user={user} setUser={setUser} />
+            <Routes>
+              <Route path="/" element={<Apod />} />
+              <Route path="/posts" element={<Posts />} />
+              <Route path="/search" element={<SearchArchive />} />
+            </Routes>
+          </>
+          :
+          <AuthPage setUser={setUser} />
+      }
     </main>
   );
 }

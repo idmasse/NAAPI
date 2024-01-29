b7b4fbac20a301ae3d125b79dc8a9ed20869fb76
ianm <idmasse@icloud.com>
2024-01-29 12:41:48 -0800

    updates navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,31 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
+import Logo from "../../components/Logo/Logo"
+import "./NavBar.css"
 
 export default function NavBar({ user, setUser }) {
 
   function handleLogOut() {
-      userService.logOut()
-      setUser(null)
-    }
+    userService.logOut()
+    setUser(null)
+  }
 
-    return (
-      <nav>
-        <Link to="/orders">Order History</Link>
-        &nbsp; | &nbsp;
-        <Link to="/orders/new">New Order</Link>
-        &nbsp; | &nbsp;
-        Hello {user.name}
-        &nbsp; | &nbsp;
-        <Link to="/" onClick={handleLogOut}>Logout</Link>
-      </nav>
-    )
+  return (
+    <nav className="navbar">
+      <Logo />
+      <div className="navbar-links">
+      <Link to="/">Home</Link>
+        
+        <Link to="/posts">Posts</Link>
+
+        <Link to="/search">Search</Link>
+
+        <Link to="/profile">Profile</Link>
+
+      </div>
+      {/* Hello {user.name}
+        &nbsp; | &nbsp; */}
+      <Link to="/" onClick={handleLogOut}>Logout</Link>
+    </nav>
+  )
 }

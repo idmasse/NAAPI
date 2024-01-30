c3171a0db54ee5e6cae6f9e090506d0522bff64a
ianm <idmasse@icloud.com>
2024-01-30 14:14:03 -0800

    adds routes for get and save apod data

--- routes/api/apod.js
+++ routes/api/apod.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const apodCtrl = require('../../controllers/api/apod')
 
 router.get('/', apodCtrl.getApod)
+router.post('/save', apodCtrl.saveApod)
 
 module.exports = router

8cc201a7150e9fc887957f21d5735a1865d94222
ianm <idmasse@icloud.com>
2024-01-30 14:14:44 -0800

    updates apod.jsx

--- src/pages/Apod/Apod.jsx
+++ src/pages/Apod/Apod.jsx
@@ -1,39 +1,45 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
+import { getUser } from '../../utilities/users-service'
+import * as apodAPI from '../../utilities/apodAPI'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 import "./Apod.css"
 
 export default function Apod() {
     const [Apod, setApod] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(null)
+    const [selectedDate, setSelectedDate] = useState(new Date())
+    const user = getUser()
 
     useEffect(() => {
         const fetchApod = async () => {
-            try {
-                const res = await fetch('/api/apod')
-                if (!res.ok) {
-                    throw new Error(`HTTP Error: ${res.status}`)
-                }
-                const apodData = await res.json()
-                setApod(apodData)
-            } catch (error) {
-                setError(error.message)
-            } finally {
-                setIsLoading(false)
-            }
+            const apodData = await apodAPI.fetchApod(selectedDate)
+            setApod(apodData)
         }
         fetchApod()
-    }, [])
+    }, [selectedDate])
+
+    async function handleSaveApod() {
+        const apodToSave = await apodAPI.saveApod(Apod)
+    }
 
     return (
         <div>
-            <h1>NASA Astronomy Picture of the Day</h1>
-            {isLoading && <span className="loader"></span>}
-            {error && <p>Error: {error}</p>}
+            <h1>NASA Astronomy Picture of the Day | {Apod.date}</h1>
+            <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} />
             {Apod && (
                 <div>
-                    <h2>{Apod.title} {Apod.date}</h2>
-                    <img className="apod-image" src={Apod.hdurl} alt={Apod.title} />
+                    <h2>{Apod.title} </h2>
+                    <a href={Apod.hdurl} target="_blank" title={Apod.title} rel="noreferrer">
+                        <img className="apod-image" src={Apod.url} alt={Apod.title} />
+                    </a>
                     <p>{Apod.explanation}</p>
+                    {/* <p>Media credit: {Apod.copyright}</p> */}
+                    {user ? (
+                        <button onClick={handleSaveApod}>Save</button>
+                    ) : (
+                        <button>Login</button>
+                    )
+                    }
                 </div>
             )}
         </div>

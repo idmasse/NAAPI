7be129f690726d72ce4a9c0cbb25049c4d40e50e
ianm <idmasse@icloud.com>
2024-01-31 15:28:48 -0800

    updates profile.jsx

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,3 +1,26 @@
+import { useState, useEffect } from 'react'
+import { getUser } from '../../utilities/users-service'
+import ProfileInfo from '../../components/ProfileInfo/ProfileInfo'
+import SavedApods from '../../components/SavedApods/SavedApods'
+import * as apodAPI from '../../utilities/apodAPI'
+import "./Profile.css"
+
 export default function Profile() {
-    return <h2>Profile</h2>
+    const [savedApods, setSavedApods] = useState([])
+    const user = getUser()
+
+    useEffect(() => {
+        const fetchSavedApods = async () => {
+                const savedApodsData = await apodAPI.getSavedApod()
+                setSavedApods(savedApodsData);
+        }
+        fetchSavedApods()
+    }, [])
+
+    return (
+        <>
+            <ProfileInfo user={user} />
+            <SavedApods savedApods={savedApods} />
+        </>
+    )
 }

ae03665e6751fe79596f247f36cc7a3cd003d0a9
ianm <idmasse@icloud.com>
2024-01-31 15:26:05 -0800

    adds routes for saving and posting apods

--- routes/api/apod.js
+++ routes/api/apod.js
@@ -4,5 +4,7 @@ const apodCtrl = require('../../controllers/api/apod')
 
 router.get('/', apodCtrl.getApod)
 router.post('/save', apodCtrl.saveApod)
+router.get('/savedApods', apodCtrl.getSavedApod)
+router.post('/postapod', apodCtrl.postApod)
 
 module.exports = router

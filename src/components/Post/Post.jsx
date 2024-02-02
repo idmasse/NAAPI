e223a8a5252532d45169cf0ffa0d75a547c6a40d
ianm <idmasse@icloud.com>
2024-02-02 00:15:34 -0800

    updates post component to display username correctly

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,12 +1,11 @@
 import './Post.css'
 
-export default 
-function Post({ user, postedApods }) {
+export default function Post({ user, postedApods }) {
     return (
         <div>
             {user && postedApods.map((apod, index) => (
                 <div key={index} className="posted-apod">
-                    <h2>{user.name}</h2>
+                    <h2>{apod.user.name}</h2>
                     <h3>{apod.title}</h3>
                     <img className="post-image" src={apod.url} alt={apod.title} />
                 </div>

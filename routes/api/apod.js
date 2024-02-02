9f681fad0ab71b8ea24d761643b611c6d95a9d5a
ianm <idmasse@icloud.com>
2024-02-02 00:14:09 -0800

    adds route for getting all posted apods

--- routes/api/apod.js
+++ routes/api/apod.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const apodCtrl = require('../../controllers/api/apod')
+const postCtrl = require('../../controllers/api/post')
 
 router.get('/', apodCtrl.getApod)
 router.post('/save', apodCtrl.saveApod)
-router.get('/savedApods', apodCtrl.getSavedApod)
-router.post('/post', apodCtrl.postApod)
-router.get('/postedapods', apodCtrl.getPostedApod)
+router.get('/savedapods', apodCtrl.getSavedApod)
+router.post('/post', postCtrl.postApod)
+router.get('/postedapods', postCtrl.getPostedApod)
+router.get('/postedapods/all', postCtrl.getAllPostedApods)
 
 module.exports = router

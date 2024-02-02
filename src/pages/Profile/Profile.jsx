033b0aeb99fa8ace00bbe9aba005238d066834d6
ianm <idmasse@icloud.com>
2024-02-01 17:33:58 -0800

    updates profile page to display posted apods

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,25 +2,36 @@ import { useState, useEffect } from 'react'
 import { getUser } from '../../utilities/users-service'
 import ProfileInfo from '../../components/ProfileInfo/ProfileInfo'
 import SavedApods from '../../components/SavedApods/SavedApods'
+import PostedApods from '../../components/PostedApods/PostedApods'
 import * as apodAPI from '../../utilities/apodAPI'
 import "./Profile.css"
 
 export default function Profile() {
     const [savedApods, setSavedApods] = useState([])
+    const [postedApods, setPostedApods] = useState([])
     const user = getUser()
 
     useEffect(() => {
         const fetchSavedApods = async () => {
                 const savedApodsData = await apodAPI.getSavedApod()
-                setSavedApods(savedApodsData);
+                setSavedApods(savedApodsData)
         }
         fetchSavedApods()
+
+        const fetchPostedApods = async () => {
+            const postedApodsData = await apodAPI.getPostedApods()
+            setPostedApods(postedApodsData)
+        }
+        fetchPostedApods()
     }, [])
 
     return (
         <>
             <ProfileInfo user={user} />
+            <h3>Saved APODs</h3>
             <SavedApods savedApods={savedApods} />
+            <h3>Posted APODs</h3>
+            <PostedApods postedApods={postedApods}/>
         </>
     )
 }

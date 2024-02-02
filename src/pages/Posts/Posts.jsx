aa3a21f9ec2478f4ae203764e1e30c376c4d1cc4
ianm <idmasse@icloud.com>
2024-02-02 00:19:47 -0800

    updated to use postAPI file

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,27 +1,20 @@
 import { useState, useEffect } from 'react'
 import { getUser } from '../../utilities/users-service'
 import Post from '../../components/Post/Post'
-import * as apodAPI from '../../utilities/apodAPI'
+import * as postAPI from '../../utilities/postAPI'
 import './Posts.css'
 
 export default function Posts() {
     const [postedApods, setPostedApods] = useState([])
     const user = getUser()
 
+    useEffect(() => {
+        const fetchPostedApods = async () => {
+            const postedApodData = await postAPI.getAllPostedApods()
+            setPostedApods(postedApodData)
+        }
+        fetchPostedApods()
+    }, [])
 
-useEffect(() => {
-    const fetchPostedApods = async () => {
-        const postedApodData = await apodAPI.getPostedApods()
-        setPostedApods(postedApodData)
-    }
-    fetchPostedApods()
-}, [])
-
-
-    return (
-        <>
-            <h1>Posts</h1>
-            <Post user={user} postedApods={postedApods} />
-        </>
-    )
+    return <Post user={user} postedApods={postedApods} />
 }

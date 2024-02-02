3c36b57e6c0dd8ed70223f989fff79ac76aa9210
ianm <idmasse@icloud.com>
2024-02-01 17:33:38 -0800

    updates posts.jsx

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from 'react'
+import { getUser } from '../../utilities/users-service'
+import Post from '../../components/Post/Post'
+import * as apodAPI from '../../utilities/apodAPI'
+import './Posts.css'
+
 export default function Posts() {
-    return <h1>Posts</h1>
+    const [postedApods, setPostedApods] = useState([])
+    const user = getUser()
+
+
+useEffect(() => {
+    const fetchPostedApods = async () => {
+        const postedApodData = await apodAPI.getPostedApods()
+        setPostedApods(postedApodData)
+    }
+    fetchPostedApods()
+}, [])
+
+
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post user={user} postedApods={postedApods} />
+        </>
+    )
 }

61d0c221c679c7b464ba20fb5e4ca0fcfc45b886
ianm <idmasse@icloud.com>
2024-02-02 00:16:26 -0800

    reorders home page before login page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
-import { Routes, Route } from 'react-router-dom';
-import { getUser } from '../../utilities/users-service';
-import AuthPage from '../AuthPage/AuthPage';
-import NavBar from '../../components/NavBar/NavBar';
-import Apod from "../Apod/Apod";
-import Posts from '../Posts/Posts';
-import SearchArchive from '../SearchArchive/SearchArchive';
-import Profile from '../Profile/Profile';
-import './App.css';
+import { useState } from "react"
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { getUser } from '../../utilities/users-service'
+import Logo from "../../components/Logo/Logo"
+import AuthPage from '../AuthPage/AuthPage'
+import NavBar from '../../components/NavBar/NavBar'
+import Apod from "../Apod/Apod"
+import Posts from '../Posts/Posts'
+import SearchArchive from '../SearchArchive/SearchArchive'
+import Profile from '../Profile/Profile'
+import './App.css'
+import '../../components/NavBar/NavBar.css'
+import '../../components/Logo/Logo.css'
 
 export default function App() {
   const [user, setUser] = useState(getUser())
+  const navigate = useNavigate()
 
   return (
     <main className="App">
@@ -20,14 +24,28 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               <Route path="/" element={<Apod />} />
+              <Route path="/login" element={<Apod />} />
               <Route path="/posts" element={<Posts />} />
               <Route path="/search" element={<SearchArchive />} />
               <Route path="/profile" element={<Profile />} />
             </Routes>
           </>
           :
-          <AuthPage setUser={setUser} />
+          <>
+            <nav className="navbar">
+              <Logo />
+              <div className="navbar-links">
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+              </div>
+            </nav>
+            <Routes>
+              <Route path="/" element={<Apod />} />
+              <Route path="/login" element={<AuthPage setUser={setUser} navigate={navigate} />} />
+            </Routes>
+          </>
       }
     </main>
-  );
+  )
 }
+

4c07ffc449e479f081c72bed8107d136013159bf
ianm <idmasse@icloud.com>
2024-02-01 16:29:45 -0800

    updates profile model

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema ({
     user: { type: Schema.Types.ObjectId, ref: 'User', unique: true, required: true },
     savedApods: [{ type: Schema.Types.ObjectId, ref: 'Apod'}],
-    sharedApods: [{ type: Schema.Types.ObjectId, ref: 'Apod'}],
+    postedApods: [{ type: Schema.Types.ObjectId, ref: 'Apod'}],
 }, {
     timestamps: true 
 })

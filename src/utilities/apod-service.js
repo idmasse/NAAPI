0d4c451b3c75bb2a212dd8d5e0ffc4eeed93880b
ianm <idmasse@icloud.com>
2024-02-02 00:57:40 -0800

    updates apod-service

--- src/utilities/apod-service.js
+++ src/utilities/apod-service.js
@@ -1,8 +1,6 @@
-const NASA_APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
-
 async function getNasaData(date) {
     try {
-        const url = `${NASA_APOD_ENDPOINT}?api_key=${process.env.NASA_API_KEY}&date=${date}`
+        const url = `${process.env.NASA_APOD_ENDPOINT}?api_key=${process.env.NASA_API_KEY}&date=${date}`
         const response = await fetch(url)
         if (!response.ok) {
             throw new Error(`NASA API Error: ${response.status}`)

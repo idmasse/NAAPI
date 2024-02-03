792888e99e80ebe7c68a1df1ea7e4f2d080cefca
ianm <idmasse@icloud.com>
2024-02-03 15:37:06 -0800

    links posted apods to their detail page

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,17 +1,20 @@
+import { Link } from 'react-router-dom'
 import './Post.css'
 
 export default function Post({ user, postedApods }) {
-    return (
-      <div className="post-container">
-        {postedApods.map((apod, index) => (
-          <div key={index} className="posted-apod">
-            <div className="title-container">
-              <h2>{apod.user.name}</h2>
-              <h3>{apod.title}</h3>
-            </div>
-            <img className="post-image" src={apod.url} alt={apod.title} />
+  return (
+    <div className="post-container">
+      {postedApods.map((apod, index) => (
+        <div key={index} className="posted-apod">
+          <div className="title-container">
+            <h2>{apod.user.name}</h2>
+            <h3>{apod.title}</h3>
           </div>
-        ))}
-      </div>
-    )
-  }
+          <Link to={`/apods/${apod._id}`} key={apod._id}>
+            <img className="post-image" src={apod.url} alt={apod.title} />
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
+}

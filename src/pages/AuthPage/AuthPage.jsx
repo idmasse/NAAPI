44454d66cd2b187150651adc8aa6e11e54b36f26
ianm <idmasse@icloud.com>
2024-02-02 18:04:34 -0800

    updates authpage css

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,12 @@
-import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import SignUpForm from "../../components/SignUpForm/SignUpForm"
 import LoginForm from "../../components/LoginForm/LoginForm"
+import './AuthPage.css'
 
 export default function AuthPage({ setUser }) {
    return (
-      <main>
+      <main className="AuthPage">
+         {/* <h1>Nasa Astronomy API Explorer</h1> */}
+
          <SignUpForm setUser={setUser}/>
          <LoginForm setUser={setUser}/>
       </main>

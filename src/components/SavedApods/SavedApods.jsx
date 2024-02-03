88d2388c7c3f52bf1eefda471c687b6a92fe2db6
ianm <idmasse@icloud.com>
2024-02-03 15:11:39 -0800

    updates css

--- src/components/SavedApods/SavedApods.jsx
+++ src/components/SavedApods/SavedApods.jsx
@@ -1,11 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import './SavedApods.css'
 
 export default function SavedApods({ savedApods }) {
     return (
         <div className='saved-apods-container'>
             {savedApods.map(apod => (
-                <Link to={`/apods/${apod._id}`} key={apod._id} className='saved-apod-link'>
+                <Link to={`/apods/${apod._id}`} key={apod._id}>
                     <div className='saved-apod'>
                         <img src={apod.url} alt={apod.title} />
                     </div>

5d34e0228eae174d80fab591fba9b682fa414643
ianm <idmasse@icloud.com>
2024-02-02 18:03:41 -0800

    updates signup form

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { signUp } from "../../utilities/users-service.js";
 
 export default function SignUpFormFunc({ setUser }) {
-    const [ formData, setFormData ] = useState({
+    const [formData, setFormData] = useState({
         name: '',
         email: '',
         password: '',
@@ -12,7 +12,7 @@ export default function SignUpFormFunc({ setUser }) {
 
     function handleChange(evt) {
         setFormData({
-            ...formData, 
+            ...formData,
             [evt.target.name]: evt.target.value,
             error: ''
         })
@@ -21,34 +21,45 @@ export default function SignUpFormFunc({ setUser }) {
     async function handleSubmit(evt) {
         evt.preventDefault()
         try {
-            const formDataPayload = {...formData}
+            const formDataPayload = { ...formData }
             delete formDataPayload.error
             delete formDataPayload.confirm
             const user = await signUp(formDataPayload)
             setUser(user)
-        } catch(err) {
+        } catch (err) {
             console.log(err)
             setFormData({ error: 'Sign-Up failed - Try again' })
         }
     }
-    
+
     const disable = formData.password !== formData.confirm;
+
     return (
-        <div>
+        <div className="form-wrapper">
             <div className="form-container">
-              <form autoComplete="off" onSubmit={handleSubmit}>
-                <label>Name</label>
-                <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-                <label>Email</label>
-                <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-                <label>Password</label>
-                <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-                <label>Confirm</label>
-                <input type="password" name="confirm" value={formData.confirm} onChange={handleChange} required />
-                <button type="submit" disabled={disable}>SIGN UP</button>
-              </form>
+                <h2>Signup</h2>
+                <p className="error-message">{formData.error}</p>
+                <form autoComplete="off" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>Name:</label>
+                        <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Email:</label>
+                        <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Password:</label>
+                        <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Confirm:</label>
+                        <input type="password" name="confirm" value={formData.confirm} onChange={handleChange} required />
+                    </div>
+                    <button type="submit" disabled={disable}>SIGN UP</button>
+                </form>
             </div>
-            <p className="error-message">&nbsp;{formData.error}</p>
-          </div>
+
+        </div>
     )
 }
